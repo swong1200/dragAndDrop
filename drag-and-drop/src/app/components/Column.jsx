e1b0c82316b36a1bfb00c0ea54f83402f2d0e476
Stuart Wong <swong31@gmail.com>
2024-07-01 17:57:28 -0700

    Added drag and drop functionality so that the rows will remain in their new position when moved

--- drag-and-drop/src/app/components/Column.jsx
+++ drag-and-drop/src/app/components/Column.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { SortableContext, verticalListSortingStrategy } from '@dnd-kit/sortable';
 import Row from './Row';
 
-export default function Column({ tasks }) {
+export default function Column({ locations }) {
   return (
     <div className="bg-offwhite rounded p-15 w-4/5 max-w-lg flex flex-col gap-15">
-      <SortableContext items={tasks} strategy={verticalListSortingStrategy}>
-        {tasks.map((task) => (
-          <Row id={task.id} title={task.title} key={task.id}/>
+      <SortableContext items={locations} strategy={verticalListSortingStrategy}>
+        {locations.map((location) => (
+          <Row id={location.id} title={location.title} key={location.id}/>
         ))}
       </SortableContext>
     </div>
--- drag-and-drop/src/app/page.js
+++ drag-and-drop/src/app/page.js
@@ -3,10 +3,11 @@
 import React, {useState} from 'react';
 import {DndContext, closestCorners} from '@dnd-kit/core';
 import Column from './components/Column'
+import { arrayMove } from '@dnd-kit/sortable';
 
 
 export default function Home() {
-    const [tasks, setTasks] = useState([
+    const [locations, setLocations] = useState([
         { id: 1, title: "Scotland Island", location: "Sydney, Australia", image: ""},
         { id: 2, title: "The Charles Grand Brasserie & Bar", location: "Lorem ipsum, Dolor", image: ""},
         { id: 3, title: "Bridge Climb", location: "Dolor, Sit amet", image: ""},
@@ -15,10 +16,25 @@ export default function Home() {
         { id: 6, title: "Vivid Festival", location: "Sydney, Australia", image: ""},
     ]);
 
+    const getLocationPos = id => locations.findIndex(location => location.id === id)
+
+    const handleDragEnd = (event)=> {
+        const {active, over} = event;
+
+        if (active.id === over.id) return;
+
+        setLocations(location => {
+            const originalPos = getLocationPos(active.id);
+            const newPos = getLocationPos(over.id);
+
+            return arrayMove(locations, originalPos, newPos)
+        })
+    }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-       <DndContext collisionDetection={closestCorners}>
-        <Column tasks={tasks}/>
+       <DndContext onDragEnd={handleDragEnd} collisionDetection={closestCorners}>
+        <Column locations={locations}/>
        </DndContext>
     </main>
   );
