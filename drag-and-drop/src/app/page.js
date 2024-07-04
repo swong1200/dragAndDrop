32e499612aad67c5a381b3f19a8b45c37e52315e
Stuart Wong <swong31@gmail.com>
2024-07-04 11:43:24 -0700

    Moved data to separate file

--- drag-and-drop/src/app/page.js
+++ drag-and-drop/src/app/page.js
@@ -4,17 +4,11 @@ import React, {useState} from 'react';
 import {DndContext, closestCorners} from '@dnd-kit/core';
 import Column from './components/Column'
 import { arrayMove } from '@dnd-kit/sortable';
+import { data } from '@/data';
 
 
 export default function Home() {
-    const [locations, setLocations] = useState([
-        { id: 1, title: "Scotland Island", location: "Sydney, Australia", image: ""},
-        { id: 2, title: "The Charles Grand Brasserie & Bar", location: "Lorem ipsum, Dolor", image: ""},
-        { id: 3, title: "Bridge Climb", location: "Dolor, Sit amet", image: ""},
-        { id: 4, title: "Scotland Island", location: "Sydney, Australia", image: ""},
-        { id: 5, title: "Clam Bar", location: "Etcetera, Vidi vici", image: ""},
-        { id: 6, title: "Vivid Festival", location: "Sydney, Australia", image: ""},
-    ]);
+    const [locations, setLocations] = useState(data);
 
     const getLocationPos = id => locations.findIndex(location => location.id === id)
 
---
+++ drag-and-drop/src/data.js
@@ -0,0 +1,8 @@
+export const data = [
+    { id: 1, title: "Scotland Island", location: "Sydney, Australia", image: ""},
+    { id: 2, title: "The Charles Grand Brasserie & Bar", location: "Lorem ipsum, Dolor", image: ""},
+    { id: 3, title: "Bridge Climb", location: "Dolor, Sit amet", image: ""},
+    { id: 4, title: "Scotland Island", location: "Sydney, Australia", image: ""},
+    { id: 5, title: "Clam Bar", location: "Etcetera, Vidi vici", image: ""},
+    { id: 6, title: "Vivid Festival", location: "Sydney, Australia", image: ""}
+]
