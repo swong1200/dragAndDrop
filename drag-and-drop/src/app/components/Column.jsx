d087a7efba1fcef789167532d7fb4ea9388151f2
Stuart Wong <swong31@gmail.com>
2024-07-29 15:04:49 -0700

    Added ability to grab a clone of the row and drag it while the original goes ghost and stays behind, but other rows still slide into place on their own.  Plus clone gets larger when it goes past a certain point now.

--- drag-and-drop/src/app/components/Column.jsx
+++ drag-and-drop/src/app/components/Column.jsx
@@ -4,10 +4,10 @@ import Row from './Row';
 
 export default function Column({ locations }) {
   return (
-    <div className="bg-offwhite rounded p-15 w-4/5 max-w-lg flex flex-col gap-15">
+    <div className="rounded p-15 w-4/5 max-w-lg flex flex-col gap-15">
       <SortableContext items={locations} strategy={verticalListSortingStrategy}>
         {locations.map((location) => (
-          <Row id={location.id} title={location.title} key={location.id} image={location.image}/>
+          <Row id={location.id} title={location.title} key={location.id} image={location.image} place={location.location} />
         ))}
       </SortableContext>
     </div>
--- drag-and-drop/src/app/components/Row.jsx
+++ drag-and-drop/src/app/components/Row.jsx
@@ -1,22 +1,68 @@
+// import React from "react";
+
+// import { useSortable } from "@dnd-kit/sortable";
+// import { CSS } from "@dnd-kit/utilities";
+
+// export default function Row({ id, title, image, place }) {
+//     const {attributes, listeners, setNodeRef, transform, transition} = useSortable({id});
+
+//     const style = {
+//         transition,
+//         transform: CSS.Transform.toString(transform)
+//     };
+
+//     return (
+//       <div ref={setNodeRef} {...attributes} {...listeners} style={style} className="bg-white rounded shadow-md w-full p-20 flex items-center justify-start gap-20 touch-none">
+//         <div className="h-96 w-96">
+//             <img src={image} className="rounded-xl " />
+//         </div>
+//         <div className="">
+//             <p>{title}</p>
+//             <p>{place}</p>
+//         </div>
+        
+//       </div>
+//     );
+//   }
+
 import React from "react";
 
 import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
 
-export default function Row({ id, title, image }) {
-    const {attributes, listeners, setNodeRef, transform, transition} = useSortable({id});
+export default function Row({ id, title, image, place }) {
+    const {attributes, listeners, setNodeRef, transform, transition, isDragging} = useSortable({id});
 
     const style = {
         transition,
-        transform: CSS.Transform.toString(transform)
+        transform: CSS.Transform.toString(transform),
+        height: isDragging ? "20px" : "400px",
     };
 
+    const ghostStyle = {
+        opacity: 0.5
+    }
+
     return (
-      <div ref={setNodeRef} {...attributes} {...listeners} style={style} className="bg-white rounded shadow-md w-full p-20 flex items-center justify-start gap-20 touch-none">
-        <div className="h-96 w-96">
-            <img src={image} className="rounded-xl" />
+        <div>
+            <div ref={setNodeRef} {...attributes} {...listeners} style={style} className="cursor-move bg-white w-568 flex justify-items-start gap-24 touch-none z-0">
+                <div className="h-96 w-96">
+                    <img src={image} className="rounded-xl h-auto max-w-full" />
+                </div>
+                <div>
+                    <p>{title}</p>
+                    <p>{place}</p>
+                </div>
+            </div>
+            {isDragging && <div style={ghostStyle} className="cursor-move bg-white w-568 flex justify-items-start gap-24 touch-none">
+                <div >
+                    <img src={image} />
+                </div>
+                <div>
+                    <p>{title}</p>
+                    <p>{place}</p>
+                </div></div>}
         </div>
-        {title}
-      </div>
+      
     );
   }
